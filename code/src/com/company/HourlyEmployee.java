da9d1c894f1380f7bd46dd382e1d24cb938e6298
ShaquanTurner <92876529+ShaquanTurner@users.noreply.github.com>
2022-03-29 21:07:10 -0400

    update

--- code/src/com/company/HourlyEmployee.java
+++ code/src/com/company/HourlyEmployee.java
@@ -1,4 +1,5 @@
 package com.company;
+//Shaquan Turner 1817001436 //Chevon McIntosh 2017001610 //Jahvaughn Binns 2017001833
 
 public class HourlyEmployee extends   Employee{
     private double wage;
